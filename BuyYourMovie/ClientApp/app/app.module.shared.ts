deb9158269c0ee39b87bd4d088c9dc01459b7293
Morzkat <felix.david21@outlook.com>
2017-10-22 23:20:01 -0400

    connectio with json test

--- BuyYourMovie/ClientApp/app/app.module.shared.ts
+++ BuyYourMovie/ClientApp/app/app.module.shared.ts
@@ -1,34 +1,51 @@
+//Angular Components
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
+//App Components
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+//App services
+import { UserService } from './services/user/user.service';
+
+//routes of the app 
+const appRoutes:Routes =
+[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+    { path: '**', redirectTo: 'home' }
+]
+
 @NgModule({
-    declarations: [
+    declarations:
+    [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
         HomeComponent
     ],
-    imports: [
+
+    imports:
+    [
         CommonModule,
         HttpModule,
         FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
+        RouterModule.forRoot(appRoutes)
+    ],
+
+    providers:
+    [
+        UserService
     ]
 })
 export class AppModuleShared {
--- BuyYourMovie/ClientApp/app/components/counter/counter.component.ts
+++ BuyYourMovie/ClientApp/app/components/counter/counter.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+//Angular Components
+import { Component, OnInit } from '@angular/core';
 
-@Component({
+//App services
+import { UserService } from './../../services/user/user.service';
+
+@Component
+({
     selector: 'counter',
     templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+
+export class CounterComponent implements OnInit
+{
+    posts: Array<Post>;
+
     public currentCount = 0;
 
-    public incrementCounter() {
+    public incrementCounter()
+    {
         this.currentCount++;
     }
+
+    constructor(private userService:UserService)
+    {
+
+    }
+
+    ngOnInit()
+    {
+        this.userService.getPosts().subscribe((post) =>
+        {
+            this.posts = post;
+        });
+    }
+}
+
+interface Post
+{
+
+    id: number,
+    body: string,
+    title: string,
+    userId: number
 }
