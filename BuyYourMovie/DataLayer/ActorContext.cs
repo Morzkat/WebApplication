aeaef1b9bf55b42f75331183f88c0015fda39f7c
Morzkat <felix.david21@outlook.com>
2017-10-29 16:11:29 -0400

    created the models for the movies and the actors

---
+++ BuyYourMovie/DataLayer/ActorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BuyYourMovie.Models;
+
+namespace BuyYourMovie.DataLayer
+{
+    public class ActorContext : DbContext
+    {
+        public ActorContext(DbContextOptions<ActorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ActorsModel> Actors { get; set; }
+    }
+}
---
+++ BuyYourMovie/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyYourMovie.Models
+{
+    public class MovieModel
+    {
+        public int id { get; set; }
+        public string movieName { get; set; }
+    }
+}
